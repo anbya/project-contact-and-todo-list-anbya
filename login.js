6f96fa96e91a6592f16c9f6152b2c9fe619f6989
anbya <anbyaalibia@gmail.com>
2019-09-07 10:46:39 +0700

    a

--- login.js
+++ login.js
@@ -15,3 +15,29 @@ $("#signInBtn").click(function(e) {
         window.location = "index.html";
     }
 });
+
+$(document).ready(function() { 
+    $("#signUp").submit(function(e) { 
+        e.preventDefault();
+        let pass1 = document.getElementById("userPass1").value;
+        let pass2 = document.getElementById("userPass2").value;
+        if(pass1 == pass2){
+            $.ajax({ 
+                'url': 'https://transdeal.co.id/eksapi/project_sign_up.php', 
+                'type': 'POST',
+                'data': $('#signUp').serialize(),
+                'cache': false,
+                'contentType': false,
+                'processData': false,
+                'async': false,
+                'success': function(){ 
+                alert("You've been registered succesfully please login");
+                $('#modal_sign_up').modal('hide');
+                }
+            });
+        }
+        else{
+            alert("You're password didn't match");
+        }
+    }); 
+}); 
